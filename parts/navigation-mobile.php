ffd415542a51bdfb900bc1bca178b1a8ab2c7134
kradyfatty <67622951+kradyfatty@users.noreply.github.com>
2021-08-03 19:10:04 +0800

    wew

---
+++ parts/navigation-mobile.php
@@ -0,0 +1,36 @@
+<div class="container">
+    <a class="navbar-logo" href="https://14thpnhrsweekcelebration.dost9.ph/register.php">
+        <img src="assets/images/logo-long-l.png" alt="" class=" logo-dark" height="50">
+        <img src="assets/images/logo-long-l.png" alt="" class=" logo-light" height="50">
+    </a>
+
+    <button type="button" class="btn btn-sm px-3 font-size-16 d-lg-none header-item waves-effect waves-light" data-toggle="collapse" data-target="#topnav-menu-content">
+        <i class='bx bx-list-ul'></i>
+    </button>
+
+    <div class="collapse navbar-collapse" id="topnav-menu-content">
+        <ul class="navbar-nav ms-auto" id="topnav-menu">
+        
+            <li class="nav-item">
+                <a class="nav-link" href="#features">Features</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#roadmap">Roadmap</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#team">Team</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#news">News</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#faqs">FAQs</a>
+            </li>
+
+        </ul>
+
+        <!-- <div class="ml-lg-2">
+            <a href="#" class="btn btn-outline-success w-xs" @click="view('island')">Back</a>
+        </div> -->
+    </div>
+</div>
